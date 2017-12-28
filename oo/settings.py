6088c9ba77d460e842f7b9c4fa2810a5a98161f2
mixyz <mixyz@protonmail.com>
2017-12-28 23:08:17 +0000

    add primary infinity apps

--- oo/settings.py
+++ oo/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'oo.meta',
+    'oo.core',
+    'oo.users',
+    'oo.transactions',
 ]
 
 MIDDLEWARE = [
